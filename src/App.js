1bc9f53eba4b75294df29394d2a4eb7f9463240d
Reid <touyalin@gmail.com>
2023-07-02 12:26:38 +0800

    add missing class

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const PackingList = () => {
 
 const State = () => {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packed X (X %)</em>
     </footer>
   );
