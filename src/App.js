3d62ccae14b6b5578c964a8f1990ba057c8cb40a
Reid <touyalin@gmail.com>
2023-07-02 12:06:39 +0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
-function App() {}
+function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <State />
+    </div>
+  );
+}
 
+const Logo = () => {
+  return <h1>Far Away</h1>;
+};
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+};
+
+const PackingList = () => {
+  return <div className="list">List</div>;
+};
+
+const State = () => {
+  return (
+    <footer>
+      <em>You have X items on your list, and you already packed X (X %)</em>
+    </footer>
+  );
+};
 export default App;
