8319a29b7f13f4efd50f7c6dd110042380c1ae5c
Reid <touyalin@gmail.com>
2023-07-02 19:16:58 +0800

    6.73 controlled elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import initialItems from "./fixtures/items";
+import { v4 as uuid } from "uuid";
 
 function App() {
   return (
@@ -16,10 +18,47 @@ const Logo = () => {
 };
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: uuid(),
+    };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 };
 
